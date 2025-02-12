5adfb0a49168882c332d9c2e1f6ff16840a939d3
Miracle McMahon <339663114@GoTVDSB.ca>
2025-02-12 13:37:05 -0500

    Working on the tutorial

--- Program.cs
+++ Program.cs
@@ -2,6 +2,16 @@
 {
     internal class Program
     {
+        public static void Joke()
+        {
+            Console.WriteLine("99 little bugs in the code");
+            Thread.Sleep(500);
+            Console.WriteLine("99 little bugs.");
+            Thread.Sleep(500);
+            Console.WriteLine("Fix a bug, run it again,");
+            Thread.Sleep(500);
+            Console.WriteLine("100 little bugs in the code.");
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
